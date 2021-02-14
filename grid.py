30df155cf9e7f9dbcb48a9225132d9ac96b11527
eDyablo <eyablonsky@gmail.com>
2021-02-14 21:23:24 +0200

    test one cell when insert into 0 0 cell of empty grid

--- grid.py
+++ grid.py
@@ -1,3 +1,22 @@
 class Grid:
+    def __init__(self):
+        self.__cells = dict()
+
     def cell_count(self):
-        return 0
+        return len(self.__cells)
+
+    def size(self):
+        return self.__row_count, self.__column_count
+
+    def __setitem__(self, key, value):
+        row, column = key
+        index = self.__index(row, column)
+        self.__cells[index] = value
+        self.__row_count, self.__column_count = row + 1, column + 1
+
+    def __getitem__(self, key):
+        index = self.__index(*key)
+        return self.__cells[index]
+
+    def __index(self, row, column):
+        return f'{row}x{column}'
--- grid_test.py
+++ grid_test.py
@@ -9,3 +9,10 @@ def empty_grid():
 
 def test_no_cells_in(empty_grid):
     assert empty_grid.cell_count() == 0
+
+def test_one_cell_when_insert_into_0_0_cell_of(empty_grid):
+    grid = empty_grid
+    grid[0, 0] = 1
+    assert grid.cell_count() == 1
+    assert grid[0, 0] == 1
+    assert grid.size() == (1, 1)
