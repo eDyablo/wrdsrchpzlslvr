6a740f804066cc738bd489b2a18e0c7d5c90b94d
eDyablo <eyablonsky@gmail.com>
2021-02-14 21:39:37 +0200

    test three diagonal cells inserted into empty grid

--- grid.py
+++ grid.py
@@ -1,18 +1,20 @@
 class Grid:
     def __init__(self):
         self.__cells = dict()
+        self.__size = 0, 0
 
     def cell_count(self):
         return len(self.__cells)
 
     def size(self):
-        return self.__row_count, self.__column_count
+        return self.__size
 
     def __setitem__(self, key, value):
         row, column = key
         index = self.__index(row, column)
         self.__cells[index] = value
-        self.__row_count, self.__column_count = row + 1, column + 1
+        self.__size = max(self.__size[0], row +
+                          1), max(self.__size[1], column + 1)
 
     def __getitem__(self, key):
         index = self.__index(*key)
--- grid_test.py
+++ grid_test.py
@@ -10,9 +10,20 @@ def empty_grid():
 def test_no_cells_in(empty_grid):
     assert empty_grid.cell_count() == 0
 
+
 def test_one_cell_when_insert_into_0_0_cell_of(empty_grid):
     grid = empty_grid
     grid[0, 0] = 1
     assert grid.cell_count() == 1
     assert grid[0, 0] == 1
     assert grid.size() == (1, 1)
+
+
+def test_three_diagonal_cells_inserted_into(empty_grid):
+    grid = empty_grid
+    for i in range(2, -1, -1):
+        grid[i, i] = i
+    assert grid.cell_count() == 3
+    assert grid.size() == (3, 3)
+    for i in range(0, 3):
+        assert grid[i, i] == i
