6097f210846c934eabfae90e1f2aa3521a949742
eDyablo <eyablonsky@gmail.com>
2021-02-17 08:25:16 +0200

    combine tests

--- wrdpzl_test.py
+++ wrdpzl_test.py
@@ -1,22 +1,10 @@
-from pytest import fixture
+from pytest import mark
 from wrdpzl import find_in_string
 
 
-@fixture
-def empty_string():
-    return ''
-
-
-@fixture
-def empty_dictionary():
-    return []
-
-
-def test_no_words_found_in(empty_string, empty_dictionary):
-    found = find_in_string(empty_string, empty_dictionary)
-    assert len(found) == 0
-
-
-def test_one_letter_word_found_in_string_containing_only_the_word():
-    found = find_in_string('a', ['a'])
-    assert len(found) == 1 and 'a' in found
+@mark.parametrize('string, dictionary, words', [
+    ('', [], []),
+    ('a', ['a'], ['a']),
+])
+def test_words_from_dictionary_found_in_string(string, dictionary, words):
+    assert find_in_string(string, dictionary) == words
