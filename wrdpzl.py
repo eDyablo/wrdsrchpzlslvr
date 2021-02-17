9c34d61754bb1c423eebc49e01a79236363f6f5f
eDyablo <eyablonsky@gmail.com>
2021-02-17 09:10:44 +0200

    make prefix table

--- wrdpzl.py
+++ wrdpzl.py
@@ -1,5 +1,21 @@
-def find_in_string(string, dictionary):
+def find_in_string(string, words):
+    dictionary = words
     found = []
-    if string in dictionary:
-        found.append(string)
+    begin, end = 0, 0
+    while end <= len(string):
+        word = string[begin:end]
+        print(word)
+        if word in dictionary:
+            found.append(word)
+            begin += len(word)
+        end += 1
     return found
+
+
+def make_prefix_table(words):
+    table = {}
+    for word in words:
+        for size in range(1, len(word)):
+            table[word[0:size]] = None
+        table[word] = word
+    return table
--- wrdpzl_test.py
+++ wrdpzl_test.py
@@ -1,10 +1,21 @@
 from pytest import mark
-from wrdpzl import find_in_string
+from wrdpzl import (find_in_string, make_prefix_table)
 
 
 @mark.parametrize('string, dictionary, words', [
     ('', [], []),
     ('a', ['a'], ['a']),
+    ('ab', ['a', 'b'], ['a', 'b']),
+    ('abcd', ['ab', 'cd'], ['ab', 'cd']),
 ])
 def test_words_from_dictionary_found_in_string(string, dictionary, words):
     assert find_in_string(string, dictionary) == words
+
+
+@mark.parametrize('words, table', [
+    ([], {}),
+    (['a'], {'a': 'a'}),
+    (['ab'], {'a': None, 'ab': 'ab'}),
+])
+def test_prefix_table_made_from_words(words, table):
+    assert make_prefix_table(words) == table
